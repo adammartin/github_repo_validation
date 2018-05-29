2b8d1962097e642d5cefd002535f5cc55d800d62
adammartin <ebondark.od@gmail.com>
2018-05-28 21:08:33 -0400

    adding basics for filter rules

--- src/github_repo_validation/filters/filter_rules.py
+++ src/github_repo_validation/filters/filter_rules.py
@@ -1,6 +1,16 @@
+from github_repo_validation.filters import rules_factory
+
+
 class FilterRules:
     def __init__(self, config):
         self.config = config
+        self.rules = rules_factory.rules()
 
     def filter(self, repository_list):
-        return repository_list
+        return [self._apply_filters(repo) for repo in repository_list]
+
+    def _apply_filters(self, repo):
+        repository = dict(repo)
+        for rule in self.rules:
+            repository = rule(repository)
+        return repository
---
+++ test/filters/filter_rules_test.py
@@ -0,0 +1,42 @@
+from github_repo_validation.filters import filter_rules, rules_factory
+from unittest import mock
+
+
+COMPLIANCE = 'compliance_weight'
+
+
+def fake_rule_1(repository):
+    value = 1
+    if repository['attrib_1'] < 1:
+        value = 0.5
+    repository[COMPLIANCE] = repository.get(COMPLIANCE, 1) * value
+    return repository
+
+
+def fake_rule_2(repository):
+    value = 1
+    if repository['attrib_2'] < 1:
+        value = 0.25
+    repository[COMPLIANCE] = repository.get(COMPLIANCE, 1) * value
+    return repository
+
+CONFIG = 'blah'
+REPO_1 = { 'attrib_1': 0, 'attrib_2': 2 }
+REPO_2 = { 'attrib_1': 2, 'attrib_2': 0 }
+REPO_3 = { 'attrib_1': 0, 'attrib_2': 1 }
+RULES = [ fake_rule_1, fake_rule_2 ]
+EXPECTED_REPO_1 = { **REPO_1, COMPLIANCE: 0.5 }
+EXPECTED_REPO_2 = { **REPO_2, COMPLIANCE: 0.25 }
+EXPECTED_REPO_3 = { **REPO_3, COMPLIANCE: 0.125 }
+
+
+@mock.patch('github_repo_validation.filters.rules_factory.rules', return_value = RULES)
+def test_filter_rules_will_apply_rules_across_a_repo(mock_factory):
+    filters = filter_rules.FilterRules(CONFIG)
+    assert filters.filter([REPO_1]) == [EXPECTED_REPO_1]
+
+
+@mock.patch('github_repo_validation.filters.rules_factory.rules', return_value = RULES)
+def test_filter_rules_will_apply_rules_across_two_repos(mock_factory):
+    filters = filter_rules.FilterRules(CONFIG)
+    assert filters.filter([REPO_1, REPO_2]) == [EXPECTED_REPO_1, EXPECTED_REPO_2]
