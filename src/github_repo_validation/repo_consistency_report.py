20af224f8372f8b462cf9224d6f5618cc1e8d035
adammartin <ebondark.od@gmail.com>
2018-05-29 13:32:13 -0500

    Now I can do csv or jsonl format output

--- src/github_repo_validation/repo_consistency_report.py
+++ src/github_repo_validation/repo_consistency_report.py
@@ -1,14 +1,33 @@
 import json
 import click
 import config
+import csv
 from validation_checks import repositories_in_violation
 
 
 @click.command()
-def repo_consistency_report():
-    # SHOULD: change config to environment variables
-    with open('repos.json', 'w') as output:
-        output.write(json.dumps(repositories_in_violation(config)))
+@click.option('--print_format', type=click.Choice(['jsonl', 'csv']))
+def repo_consistency_report(print_format='jsonl'):
+    if print_format=='jsonl':
+        _print_jsonl()
+    _print_csv()
+
+
+
+def _print_jsonl():
+    with open('repos.jsonl', 'w') as output:
+        for entry in repositories_in_violation(config):
+            output.writelines(json.dumps(entry)+'\n')
+
+
+def _print_csv():
+    with open('repos.csv', 'w') as output:
+        repos = repositories_in_violation(config)
+        fieldnames = repos[0].keys()
+        writer = csv.DictWriter(output, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(repos)
+
 
 if __name__ == "__main__":
     repo_consistency_report()
